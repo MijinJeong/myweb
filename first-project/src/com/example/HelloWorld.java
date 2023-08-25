c5d22be8179efe83969f6a37f74433035fb34fb2
MijinJeong <50631248+MijinJeong@users.noreply.github.com>
2023-08-25 20:59:18 +0900

    Delete first-project directory
    왠지는 모르겠는데 잘못 올라감!
    난 myweb폴더만 올리고 싶어서 myweb폴더에서 커밋 푸쉬 했는데 이렇게 됨...

--- first-project/src/com/example/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        var name = "Dowon Lee";
-        System.out.println("Hello, " + name);
-    }
-}
