cbc9e0b4cc033a515c021db862eda2965b04f48c
Mijin <alwls1357@gmail.com>
2023-08-25 20:40:20 +0900

    commit after lesson

--- myweb/src/main/java/kr/co/joneconsulting/HiServlet.java
+++ myweb/src/main/java/kr/co/joneconsulting/HiServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet("/hi-servlet")
 public class HiServlet extends HttpServlet {
@@ -15,6 +18,13 @@ public class HiServlet extends HttpServlet {
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter out = resp.getWriter();
         out.println("<H2>Hi, there</h2>");
+
+        LocalDate _date = LocalDate.now();
+        String today = _date.toString();
+        out.println(today);
+
+        LocalDateTime _time = LocalDateTime.now();
+        out.println(_time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 
         out.close();
     }
